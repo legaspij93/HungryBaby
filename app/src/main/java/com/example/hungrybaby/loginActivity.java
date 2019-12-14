2841d11fabfc1fd993559b046dbc85844d47b2ab
jstn027 <chuajustin27@gmail.com>
2019-12-14 23:45:10 +0800

    timer

--- app/src/main/java/com/example/hungrybaby/loginActivity.java
+++ app/src/main/java/com/example/hungrybaby/loginActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -39,6 +40,11 @@ public class loginActivity extends AppCompatActivity {
         passwordInput = findViewById(R.id.passwordInput);
 
         mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+
+        if(firebaseUser!=null){
+            startActivity(new Intent(loginActivity.this, menuActivity.class));
+        }
 
         databaseUsers = FirebaseDatabase.getInstance().getReference("users");
         databaseCart = FirebaseDatabase.getInstance().getReference("cart");
--- app/src/main/java/com/example/hungrybaby/profileActivity.java
+++ app/src/main/java/com/example/hungrybaby/profileActivity.java
@@ -3,14 +3,19 @@ package com.example.hungrybaby;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,6 +25,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Locale;
+
 public class profileActivity extends AppCompatActivity {
 
     TextView userName, emailAddress, contact, address;
@@ -27,10 +34,24 @@ public class profileActivity extends AppCompatActivity {
     DatabaseReference databaseUsers;
     Button logoutBtn;
 
+    private CountDownTimer mCountDownTimer;
+    private static final long START_TIME_IN_MILLIS = 15000;
+
+    private boolean mTimerRunning;
+    private TextView mTextViewCountDown, stat;
+    private long mTimeLeftInMillis;
+    private long mEndTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        stat = findViewById(R.id.cash);
+        resetTimer();
+        startTimer();
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -41,6 +62,7 @@ public class profileActivity extends AppCompatActivity {
         contact = findViewById(R.id.contact);
         address = findViewById(R.id.address);
         logoutBtn = findViewById(R.id.logoutBtn);
+        stat = findViewById(R.id.status);
 
         userName.setText(mAuth.getCurrentUser().getDisplayName());
         emailAddress.setText(mAuth.getCurrentUser().getEmail());
@@ -106,4 +128,88 @@ public class profileActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
+        mTimeLeftInMillis = prefs.getLong("millisLeft", START_TIME_IN_MILLIS);
+        mTimerRunning = prefs.getBoolean("timerRunning", false);
+
+        updateCountDownText();
+
+        if (mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0);
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+            } else {
+                startTimer();
+            }
+        }
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+    }
+
+    private void startTimer() {
+        mEndTime = START_TIME_IN_MILLIS + mTimeLeftInMillis;
+//        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+
+
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                Toast.makeText(profileActivity.this, "Timer Finish", Toast.LENGTH_SHORT).show();
+
+
+            }
+        }.start();
+
+        mTimerRunning = true;
+    }
+
+    private void updateCountDownText() {
+        final String CHANNEL_ID = "notif";
+        final int NOTIFICATION_ID = 001;
+
+        int sec = (int) (mTimeLeftInMillis / 1000) % 60;
+        if( sec == 7)
+        {
+            stat.setText("WEW");
+            Toast.makeText(profileActivity.this, "WEW", Toast.LENGTH_SHORT).show();
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+            builder.setContentTitle("Demo App Notification");
+            builder.setContentText("New Notification From Demo App..");
+            builder.setTicker("New Message Alert!");
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+        }
+
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
 }
