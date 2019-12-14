da5c4936b60f6751afa3f677f9c1aeb52c5dfa5e
legaspij93 <47266085+legaspij93@users.noreply.github.com>
2019-12-15 02:08:21 +0800

    autofill start

--- app/src/main/java/com/example/hungrybaby/CheckoutActivity.java
+++ app/src/main/java/com/example/hungrybaby/CheckoutActivity.java
@@ -14,6 +14,7 @@ import androidx.core.app.NotificationManagerCompat;
 import com.example.hungrybaby.Model.Cart;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
@@ -22,10 +23,14 @@ import java.util.Locale;
 
 public class CheckoutActivity extends AppCompatActivity {
 
+    DatabaseReference databaseUsers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        databaseUsers = FirebaseDatabase.getInstance().getReference("users");
 
     }
 
