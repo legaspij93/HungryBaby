c1fe5ba891f83dd80a0adbf229c9da4ce92f0a2d
legaspij93 <47266085+legaspij93@users.noreply.github.com>
2019-12-16 00:53:15 +0800

    working on history indivs + fixed status

--- app/src/main/java/com/example/hungrybaby/CheckoutActivity.java
+++ app/src/main/java/com/example/hungrybaby/CheckoutActivity.java
@@ -108,6 +108,7 @@ public class CheckoutActivity extends AppCompatActivity {
                 order.setDeliverAddress(addressInput.getText().toString());
                 order.setTotalCost(cartIntent.getStringExtra("TOTAL"));
                 order.setDateOrdered(timestamp);
+                order.setStatus("Preparing Order!");
                 databaseCurrent.child(id).setValue(order);
             }
 
