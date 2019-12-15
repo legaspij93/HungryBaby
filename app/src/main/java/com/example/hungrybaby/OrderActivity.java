495a6b9964b69fbeb1432d7060879b705b9f9baf
legaspij93 <47266085+legaspij93@users.noreply.github.com>
2019-12-16 00:21:05 +0800

    current order based on db

--- app/src/main/java/com/example/hungrybaby/OrderActivity.java
+++ app/src/main/java/com/example/hungrybaby/OrderActivity.java
@@ -9,15 +9,69 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.hungrybaby.Model.Cart;
+import com.example.hungrybaby.Model.Order;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderActivity extends AppCompatActivity {
+
+    DatabaseReference databaseCurrent;
+
+    TextView deliveryAddress, subtotal, total;
+    List<Order> orders;
+    List<Cart> carts;
+
+    ListView listViewOrder;
+//    Boolean orderExists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_no_order);
+        setContentView(R.layout.activity_order);
+
+//        orderExists = true;
+        databaseCurrent = FirebaseDatabase.getInstance().getReference("currentOrder");
+
+        listViewOrder = findViewById(R.id.listViewOrder);
+
+        orders = new ArrayList<>();
+
+        deliveryAddress = findViewById(R.id.deliveryAddress);
+        subtotal = findViewById(R.id.subtotal);
+        total = findViewById(R.id.total);
+
+//        databaseCurrent.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if(dataSnapshot.hasChild("currentOrder")){
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//        if(orderExists == true){
+//            setContentView(R.layout.activity_order);
+//
+//        }else{
+//            setContentView(R.layout.activity_no_order);
+//        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -49,6 +103,56 @@ public class OrderActivity extends AppCompatActivity {
                         break;
                 }
                 return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseCurrent.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                orders.clear();
+
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting blog
+                    Order order = postSnapshot.getValue(Order.class);
+                    //adding blog to the list
+                    orders.add(order);
+                }
+//                int sum =0;
+//
+//                for(int i=0;i<carts.size();i++){
+//                    int price = Integer.parseInt(carts.get(i).getCost());
+//                    sum = sum + (price*carts.get(i).getQuantity());
+//                }
+//                String sub = String.valueOf(sum);
+//                subTotal.setText(sub);
+//                String total = String.valueOf(sum + 50);
+//                totalPrice.setText(total);
+                carts = orders.get(0).getOrders();
+                //creating adapter
+                CartList blogAdapter = new CartList(OrderActivity.this, carts);
+                //attaching adapter to the listview
+                listViewOrder.setAdapter(blogAdapter);
+
+                deliveryAddress.setText(orders.get(0).getDeliverAddress());
+                total.setText(orders.get(0).getTotalCost());
+
+                String tc = orders.get(0).getTotalCost();
+                int sub = Integer.parseInt(tc) - 50;
+                subtotal.setText(Integer.toString(sub));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
